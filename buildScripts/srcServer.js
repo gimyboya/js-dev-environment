85831bf32105b4df4f7eda32b17c54333fa3844d
Unknown <b.afatah@yahoo.com>
2017-01-16 17:37:51 +0800

    webpack
    integrating webpack for bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,17 @@
 var express = require('express');
 import path  from 'path';
 import open  from 'open';
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.development';
 
 const port = 3000;
-
 const app = express();
+const webpackCompiler = webpack(webpackConfig);
+
+app.use(require('webpack-dev-middleware')(webpackCompiler, {
+    noInfo: true,
+    publicPath: webpackCompiler.output.publicPath
+}));
 
 app.get('/', function(request, response){
 response.sendFile(path.join(__dirname, '../src/index.html'));
