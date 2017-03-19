658d7cd26d57b22d1278ad5c8f5f9a0e6d677df8
gimy boya <b.afatah@yahoo.com>
2017-03-20 00:37:24 +0800

    fixed mock api in index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import './index.css';
 import { getUsers, deleteUser } from './api/userApi';
 
 // Populate table of users via API call.
-getUsers().then(result => {
-  let usersBody = "";
+getUsers().then((result) => {
+  let usersBody = '';
 
-  result.forEach(user => {
+  result.forEach((user) => {
     usersBody += `<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
       <td>${user.id}</td>
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
       <td>${user.email}</td>
-      </tr>`
+      </tr>`;
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
@@ -22,13 +22,16 @@ getUsers().then(result => {
 
   // Must use array.from to create a real array from a DOM collection
   // getElementsByClassname only returns an "array like" object
-  Array.from(deleteLinks, link => {
-    link.onclick = function (event) {
+  Array.from(deleteLinks, (link) => {
+    const Link = link;
+    Link.onclick = (event) => {
       const element = event.target;
       event.preventDefault();
-      deleteUser(element.attributes["data-id"].value);
+      deleteUser(element.attributes['data-id'].value);
       const row = element.parentNode.parentNode;
       row.parentNode.removeChild(row);
     };
+
+    return Link;
   });
 });
