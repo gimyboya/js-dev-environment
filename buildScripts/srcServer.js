044023018c8a642d2ddd77bdca3efaa4341a4b2e
gimy boya <b.afatah@yahoo.com>
2017-03-19 09:08:36 +0800

    webpack bundling dev mod

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import webpackConfigDev from '../webpack.config.dev';
 
 const port = 3000;
 let app = express();
+let bundler = webpack(webpackConfigDev);
+
+app.use(require('webpack-dev-middleware')(bundler, {
+    publicPath: webpackConfigDev.output.publicPath
+}));
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
