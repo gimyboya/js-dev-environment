b1800c52e2ba3bfbb95624490df88e5e7bc6dc8a
gimy boya <b.afatah@yahoo.com>
2017-03-19 12:01:00 +0800

    Test and Travis CI up and running

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+//this is not transpiled
+
+//babel register should run before mocha so that our code will be transpiled
+require('babel-register')();//excuting bable first
+
+//Disable webpack features that Mocha don't understand
+require.extensions['.css'] = function(){} //it will treat .css as an empty function
