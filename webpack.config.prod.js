4efdf6ca21e9f8ee4bb3255a8349eba67bbb61ff
gimy boya <b.afatah@yahoo.com>
2017-03-20 12:57:58 +0800

    chunk bundling up and running

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,21 +1,33 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: true,
-  entry: [
-    'whatwg-fetch',
-    path.resolve(__dirname, 'src/index'),
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index'),
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundled.js',
+    filename: '[name].[chunkhash].js',
   },
   plugins: [
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
     // Create HTML file taht includes reference to build.js
     new HtmlWebpackPlugin({
       template: 'src/index.html',
